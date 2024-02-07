7d02a75010ff6f238387f3b2e271ee8426b4f001
Adel Rodríguez <hello@adelrodriguez.com>
2024-02-07 15:33:09 -0400

    refactor: minor fixes on index

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node"
 import { Link } from "@remix-run/react"
+import { route } from "routes-gen"
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,13 +11,19 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <main>
-      <h1>Illustrate your writing</h1>
-      <h2>
-        Turn your text into beautiful images to go along your blog post, your
-        novel and more
-      </h2>
-      <Link to="/create">Create your first image</Link>
+    <main className="h-full bg-black">
+      <div className="flex h-full flex-col items-center justify-center gap-y-4 text-center">
+        <h1 className="text-5xl font-black text-white">
+          Bring your words to life
+        </h1>
+        <h2 className="mt-4 text-2xl font-light text-gray-300">
+          Transform your writing into beautiful visuals to publish on your blog,
+          newsletter, book, or more.
+        </h2>
+        <Link className="text-white" to={route("/create")}>
+          Create your first image 🪄
+        </Link>
+      </div>
     </main>
   )
 }
