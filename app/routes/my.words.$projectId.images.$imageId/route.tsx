4f1f74e8359ebe18aba03216610248ef9103e0d6
Adel Rodríguez <hello@adelrodriguez.com>
2024-02-10 11:21:06 -0400

    style: spacing below title

--- app/routes/my.words.$projectId.images.$imageId/route.tsx
+++ app/routes/my.words.$projectId.images.$imageId/route.tsx
@@ -106,9 +106,9 @@ export default function Route() {
                   src={image.publicUrl ?? undefined}
                 />
 
-                <div className="flex justify-between p-6">
+                <div className="flex items-center justify-between p-6">
                   <div>
-                    <h2 className="text-lg font-semibold text-gray-600">
+                    <h2 className="mb-1 text-lg font-semibold text-gray-600">
                       {image.template.artStyle?.name}
                     </h2>
                     <p className="text-sm text-gray-500">
