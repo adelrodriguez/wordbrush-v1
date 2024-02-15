bb2ee48920e038990b8040b7e9fd1dabe58f7847
Adel Rodríguez <hello@adelrodriguez.com>
2024-02-15 10:55:14 -0400

    refactor: add exit link

--- app/routes/create/route.tsx
+++ app/routes/create/route.tsx
@@ -1,14 +1,30 @@
-import { MetaFunction } from "@remix-run/node"
-import { Outlet } from "@remix-run/react"
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node"
+import { Link, Outlet, useLoaderData } from "@remix-run/react"
+import { route } from "routes-gen"
+
+import auth from "~/helpers/auth.server"
 
 export const meta: MetaFunction = () => [
   { title: "Create a new image | 🎨 Wordbrush" },
   { content: "Bring your words to life", name: "description" },
 ]
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await auth.isAuthenticated(request)
+
+  return json({ isAuthenticated: !!user })
+}
+
 export default function Route() {
+  const { isAuthenticated } = useLoaderData<typeof loader>()
   return (
     <main className="mx-auto flex max-w-2xl flex-col items-center justify-start px-4 md:p-0">
+      <Link
+        className="absolute left-4 top-4 text-sm font-semibold leading-6 text-gray-900"
+        to={isAuthenticated ? route("/my/words") : route("/")}
+      >
+        <span aria-hidden="true">&larr;</span> Exit
+      </Link>
       <Outlet />
     </main>
   )
